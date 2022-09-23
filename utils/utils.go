bd66a2a09ee2a0ed04536fe50b5c512cdd12c119
rockjoon <49752338+rockjoon@users.noreply.github.com>
2022-09-23 23:18:10 +0900

    feat : util -> ToBytes

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,20 @@
 package utils
 
-import "log"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 func HandleError(err error) {
 	if err != nil {
 		log.Panicln(err)
 	}
+}
+
+func ToBytes(i interface{}) []byte {
+	var aBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&aBuffer)
+	HandleError(encoder.Encode(i))
+	return aBuffer.Bytes()
 }
